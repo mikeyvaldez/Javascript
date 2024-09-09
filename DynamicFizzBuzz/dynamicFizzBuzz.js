4c3ebd27b020ba883e08d13592f8899e79f5ff70
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-09-09 10:12:30 -0700

    Dynamic Fizz Buzz with numbers

--- DynamicFizzBuzz/dynamicFizzBuzz.js
+++ DynamicFizzBuzz/dynamicFizzBuzz.js
@@ -6,8 +6,22 @@
 // are divisible by num1 or num2, but not both.
 
 
+function dynamicFizzBuzz(max, num1, num2){
+    let newArr = [];
+
+    for(let i = 0; i < max; i++){
+        if(i % num1 === 0 && i % num2 !==0){
+            newArr.push(i);
+        } else if (i % num2 === 0 && i % num1 !==0){
+            newArr.push(i);
+        }
+    }
+
+    return newArr;
+}
 
 
 
 
-
+console.log(dynamicFizzBuzz(20, 3, 5));         // [3, 5, 6, 9, 10, 12, 18]
+console.log(dynamicFizzBuzz(24, 4, 6));         // [4, 6, 8, 16, 18, 20] 
