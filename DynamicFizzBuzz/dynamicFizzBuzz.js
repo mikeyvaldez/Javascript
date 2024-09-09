7f0db904d0a8d3acbe5289949b08fc2d37af68cf
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-09-08 23:09:35 -0700

    begin dynamic fizz buzz

---
+++ DynamicFizzBuzz/dynamicFizzBuzz.js
@@ -0,0 +1,13 @@
+
+
+
+// Write a function dynamicFizzBuzz that accepts three arguments (max, num1, and num2).
+// The function should return an array containing positive numbers less than max that
+// are divisible by num1 or num2, but not both.
+
+
+
+
+
+
+
