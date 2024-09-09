ac3d8f91502bd71aa8eaf99361cb502ddffcb51d
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-09-09 14:06:07 -0700

    beginning of hipsterfy script

---
+++ Hipsterfy/hipsterfy.js
@@ -0,0 +1,20 @@
+
+
+
+// Write a function hipsterfy(sentence) that takes in a sentence string
+// and returns the sentence where every word is missing its last vowel.
+
+
+
+
+
+
+
+let hispsterfy = function(sentence){
+
+}
+
+
+console.log(hispsterfy('When should everyone wake up?'));     // 'Whn shold everyon wak p?'
+console.log(hispsterfy('get ready for our bootcamp'));        // 'gt redy fr or bootcmp'
+console.log(hispsterfy('panthers are great animals'));        // 'panthrs ar gret animls'
