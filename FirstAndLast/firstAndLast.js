38f27158c66664e618edfa0a6df55ac4e422719b
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-09-09 10:29:40 -0700

    First and Last

---
+++ FirstAndLast/firstAndLast.js
@@ -0,0 +1,26 @@
+
+
+
+// Write a function firstAndLast that takes in an array of numbers and
+// returns the sum of the first and last element if there is an even number
+// of elements in the array. If there is an odd number of elements in the array,
+// then the function should return the difference between the first and last elements of the array.
+
+
+function firstAndLast(array){
+    let sum = array[0] + array[array.length-1];
+    let diff = array[0] - array[array.length-1];
+
+    if(array.length % 2 === 0){
+        return sum;
+    }
+    return diff;
+}
+
+
+
+console.log(firstAndLast([1, 2, 3, 4]));                // 5
+console.log(firstAndLast([1, 2, 3, 4, 5]));             // -4
+console.log(firstAndLast([12, 5]));                     // 17
+console.log(firstAndLast([12]));                        // 0
+console.log(firstAndLast([7, 11, 3]));                  // 4
