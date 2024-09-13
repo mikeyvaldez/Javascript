943c1a9f3ad8b7ac24fdd803ba2ab32e1b37a8e7
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-09-13 14:19:27 -0700

    Using forEach function to return sum

---
+++ AdvanceArrays/problems/00-sum-forEach.js
@@ -0,0 +1,29 @@
+/*
+Write a function `sumForEach(arr)` that accepts an array as an arg.
+The function should return the total sum of all values in the array.
+
+Solve this using Array's `forEach()` method.
+*/
+
+let sumForEach = function(arr){
+    let sum = 0; 
+    arr.forEach(function(item){
+        sum += item;
+    });        
+    return sum;
+}
+
+
+
+
+console.log(sumForEach([4, 3, -1, 10])); // 16
+console.log(sumForEach([6, 7, 2])); // 15
+console.log(sumForEach([])); // 0
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+
+try {
+  module.exports = sumForEach;
+} catch (e) {
+  module.exports = null;
+}
