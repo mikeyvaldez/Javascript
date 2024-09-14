d7148cf3189cc62ce8682a891337c0fb0db2d823
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-09-14 00:19:23 -0700

    Remove words with e using filter()

---
+++ AdvanceArrays/problems/04-remove-e-words-filter.js
@@ -0,0 +1,31 @@
+/*
+Write a function `removeEWords(sentence)` that accepts a sentence string as an
+arg. The function should return a new string, containing only the words that
+don't have the letter "e" in them.
+
+Solve this using Array's `filter()` method.
+*/
+
+
+
+let removeEWords = function(sentence){
+    let words = sentence.split(" ");
+    return words.filter(element => {
+        return !element.toLowerCase().includes('e');
+    }).join(" ");
+}
+
+
+
+
+
+console.log(removeEWords('What time is it everyone?')); // 'What is it'
+console.log(removeEWords('Enter the building')); // 'building'
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+
+try {
+  module.exports = removeEWords;
+} catch (e) {
+  module.exports = null;
+}
