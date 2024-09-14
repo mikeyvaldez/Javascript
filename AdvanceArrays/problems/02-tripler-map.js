7f63e2f460c3f0fb85e2c4b0f964c2767143b4af
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-09-13 17:53:41 -0700

    Tripler using map()

---
+++ AdvanceArrays/problems/02-tripler-map.js
@@ -0,0 +1,28 @@
+/*
+Write a function `tripler(nums)` that takes in an array as an arg. The function
+should return a new array containing three times every number of the original
+array.
+
+Solve this using Array's `map()` method.
+*/
+
+
+let tripler = function(arr){
+    return arr.map((num) => num * 3);
+}
+
+
+
+
+
+
+
+console.log(tripler([2, 7, 4])); // [ 6, 21, 12 ]
+console.log(tripler([-5, 10, 0, 11])); // [ -15, 30, 0, 33 ]
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+
+try {
+  module.exports = tripler;
+} catch (e) {
+  module.exports = null;
+}
