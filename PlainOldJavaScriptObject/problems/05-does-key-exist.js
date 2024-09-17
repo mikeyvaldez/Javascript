2c21a70966643cb48046331907708ce0415256e6
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-09-17 13:02:13 -0700

    does key exist in object

---
+++ PlainOldJavaScriptObject/problems/05-does-key-exist.js
@@ -0,0 +1,23 @@
+/***********************************************************************
+Write a function `doesKeyExist(obj, key)` that takes in an object and a
+key and returns true if the key is inside the object and false if the
+key is not inside the object.
+***********************************************************************/
+
+
+function doesKeyExist(obj, key){
+    if(obj[key] !== undefined) return true;
+    return false;
+}
+
+
+
+
+
+let obj1 = {bootcamp: 'App Academy', course: 'Bootcamp Prep'}
+console.log(doesKeyExist(obj1, 'course')); // => true
+console.log(doesKeyExist(obj1, 'name')); // => false
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+module.exports = doesKeyExist;
+
