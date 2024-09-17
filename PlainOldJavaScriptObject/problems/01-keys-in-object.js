f0b47539376bb7996f0cb852041217ced89a77be
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-09-17 09:37:19 -0700

    Plain Old Javascript Objects (POJO)

---
+++ PlainOldJavaScriptObject/problems/01-keys-in-object.js
@@ -0,0 +1,32 @@
+/***********************************************************************
+Write a function called `keysInObject(obj)` that takes in an object and returns
+an array of all the keys within that Object.
+
+Do this once using a `for...in` loop and once using `Object.keys`.
+***********************************************************************/
+
+
+function keysInObject(obj){
+    let array = [];
+
+    for(let currentKey in obj){
+        array.push(currentKey);
+    }
+
+    return array;
+}
+
+// function keysInObject(obj){
+//     return Object.keys(obj);
+// }
+
+
+
+let animals = { dog: 'Wolfie', cat: 'Jet', bison: 'Bilbo' }
+let foods = { apple: 'tart', lemon: 'sour', mango: 'sweet' }
+console.log(keysInObject(animals)); // => ["dog", "cat", "bison"]
+console.log(keysInObject(foods)); // => ["apple", "lemon", "mango"]
+
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+module.exports = keysInObject;
