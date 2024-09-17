6fef6a254abc1f1d2de62f03b541ee2c086f9c90
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-09-17 13:27:26 -0700

    break down object using spread operator

---
+++ PlainOldJavaScriptObject/problems/07-break-down-object.js
@@ -0,0 +1,27 @@
+/***********************************************************************
+Write a function `breakDownObj(obj)` that takes in an object as a parameter
+and returns an array containing:  all the keys from the object **and** all the
+values of the object.
+
+**Hint**: Use spread syntax to spread out elements into an array!
+***********************************************************************/
+
+
+
+function breakDownObj(obj){
+    return [...Object.keys(obj), ...Object.values(obj)];
+}
+
+
+
+
+let object1 = {name: 'Rupert', age: 5, speak: 'Meow'};
+console.log(breakDownObj(object1)); // => [ 'name', 'age', 'speak', 'Rupert', 5, 'Meow' ]
+
+let object2 = {location: 'NY', borough: 'Brooklyn'};
+console.log(breakDownObj(object2)); // => [ 'location', 'borough', 'NY', 'Brooklyn' ]
+
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+module.exports = breakDownObj;
+
