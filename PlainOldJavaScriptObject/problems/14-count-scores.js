62621611883101dd1e9e102d338756767e65d69f
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-09-18 14:37:38 -0700

    Count scores using object

---
+++ PlainOldJavaScriptObject/problems/14-count-scores.js
@@ -0,0 +1,48 @@
+/***********************************************************************
+Write a function `countScores(people)` that takes in an array of score
+objects (people) as its input. A score object has two key-value pairs:
+the scorer (string) and a point value (number). `countScores(people)` should
+return an object that has key-value pairs listing each person who scored as a key
+and the sum of the total points for the game as their value.
+***********************************************************************/
+
+
+function countScores(people){
+    let playersScores = {};
+
+    for(let i = 0; i < people.length; i++){
+        let obj = people[i];
+        let name = obj.name;
+        let personScore = obj.score;
+        if(playersScores[name]){
+            playersScores[name] += personScore;
+        } else {
+            playersScores[name] = personScore;
+        }
+    }
+
+    return playersScores;
+}
+
+
+
+let ppl = [{name: "Anthony", score: 10},
+    {name: "Fred", score : 10},
+    {name: "Anthony", score: -8},
+    {name: "Winnie", score: 12}];
+
+console.log(countScores(ppl)); //=> { Anthony: 2, Fred: 10, Winnie: 12 }
+
+let peeps = [
+{name: "Anthony", score: 2},
+{name: "Winnie", score: 2},
+{name: "Fred", score: 2},
+{name: "Winnie", score: 2},
+{name: "Fred", score: 2},
+{name: "Anthony", score: 2},
+{name: "Winnie", score: 2}
+];
+console.log(countScores(peeps)); //=> { Anthony: 4, Fred: 4, Winnie: 6 }
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+module.exports = countScores;
