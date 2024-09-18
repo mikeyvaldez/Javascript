53072204b2349a6892949ea0a188f3ebff5dff08
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-09-18 09:16:51 -0700

    Rest parameter syntax

---
+++ PlainOldJavaScriptObject/problems/10-rest-sum.js
@@ -0,0 +1,20 @@
+/***********************************************************************
+Write a function named `restSum` that accepts all incoming parameters and sums them.
+**Hint**: Use rest parameter syntax!
+***********************************************************************/
+
+
+
+function restSum(...otherNums){
+    return otherNums.reduce((accum, currVal) => accum + currVal);
+}
+
+
+
+
+console.log(restSum(3,5,6)); // => 14
+console.log(restSum(1, 2, 3, 4, 5, 6, 7, 8, 9)); // => 45
+console.log(restSum(0)); // => 0
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+module.exports = restSum;
