ab46eb183a7e2a171aea4fdfc6344e3ff21d691b
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-09-19 00:18:40 -0700

    multi map

---
+++ CallBacks/problems/03-multi-map.js
@@ -0,0 +1,23 @@
+/*******************************************************************************
+Write a function `multiMap` that accepts a value, a number n, and a callback.
+The function should return the new value that results from running the original value
+through the callback n times.
+*******************************************************************************/
+
+
+
+let multiMap = function(val, num, cb){
+    
+}
+
+
+
+
+let result1 = multiMap(7, 2, function(n) {
+    return n * 10;
+  });
+  
+  console.log(result1); // 700
+  
+  /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
+  module.exports = multiMap;
