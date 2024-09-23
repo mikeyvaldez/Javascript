cdb3236f82dc33c76ee2646f4f948e7619310c2b
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-09-23 08:55:20 -0700

    alternating map

--- CallBacks/problems/23-alternating-map.js
+++ CallBacks/problems/23-alternating-map.js
@@ -15,8 +15,13 @@ In other words,
 function alternatingMap(arr, cb1, cb2){
     let newArr = [];
     for(let i = 0; i < arr.length; i++){
-        
+        if(i % 2 === 0){
+            newArr.push(cb1(arr[i]));
+        } else {
+            newArr.push(cb2(arr[i]));
+        }
     }
+    return newArr;
 }
 
 
