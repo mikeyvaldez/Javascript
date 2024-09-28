d907489872c3de9697313d50c10ca003447928e7
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-09-27 22:45:56 -0700

    range using recursion

---
+++ Recursion/problems/04-range.js
@@ -0,0 +1,23 @@
+/***********************************************************************
+Write a recursive function, `range`, that takes a start and an end and returns
+an array of all numbers in that range, exclusive. If the end number is less than
+the start, return an empty array.
+***********************************************************************/
+
+let range = (start, end) => {
+    if(start >= end) return [];
+    return [start].concat(range(start+1, end))
+}
+
+
+
+console.log(range(1, 5)); // [1, 2, 3, 4]
+console.log(range(3, 4)); // [3]
+console.log(range(7, 6)); // []
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
+try {
+  module.exports = range;
+} catch (e) {
+  module.exports = null;
+}
