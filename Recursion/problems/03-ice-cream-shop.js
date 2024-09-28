50556b5a5a883883a0d856d279f3735ce8553763
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-09-27 22:30:03 -0700

    ice cream shop recursion

---
+++ Recursion/problems/03-ice-cream-shop.js
@@ -0,0 +1,29 @@
+/***********************************************************************
+Write a recursive function `iceCreamShop(flavors, favorite)` that takes in an
+array of ice cream flavors available at the ice cream shop, as well as the
+user's favorite ice cream flavor. Recursively find out whether or not the shop
+offers their favorite flavor.
+***********************************************************************/
+
+
+let iceCreamShop = (flavors, favorite) => {
+  if(!flavors.length) return false;
+  if(flavors.shift() === favorite) return true;
+  return iceCreamShop(flavors, favorite);
+}
+
+
+console.log(iceCreamShop(['vanilla', 'strawberry'], 'blue moon')); // false
+console.log(iceCreamShop(['pistachio', 'green tea', 'chocolate', 'mint chip'], 'green tea')); // true
+console.log(iceCreamShop(['cookies n cream', 'blue moon', 'superman', 'honey lavender', 'sea salt caramel'], 'pistachio')); // false
+console.log(iceCreamShop(['moose tracks'], 'moose tracks')); // true
+console.log(iceCreamShop([], 'honey lavender')); // false
+
+
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
+try {
+  module.exports = iceCreamShop;
+} catch (e) {
+  module.exports = null;
+}
