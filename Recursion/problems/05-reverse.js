4a93c6fc55b9b1241937397c3c04bde4511238c2
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-09-28 22:53:03 -0700

    beginning reverse using recursion

---
+++ Recursion/problems/05-reverse.js
@@ -0,0 +1,25 @@
+/***********************************************************************
+Write a recursive function reverse(string) that takes in a string and returns
+it reversed.
+***********************************************************************/
+
+
+let reverse = string => {
+    if(!string.length) return ""
+    
+}
+
+
+console.log(reverse("house")); // "esuoh"
+console.log(reverse("dog")); // "god"
+console.log(reverse("atom")); // "mota"
+console.log(reverse("q")); // "q"
+console.log(reverse("id")); // "di"
+console.log(reverse("")); // ""
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
+try {
+  module.exports = reverse;
+} catch (e) {
+  module.exports = null;
+}
