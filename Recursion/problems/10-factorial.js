5bb98350d863d5c60021265ebe9d9bd1e4f0f74c
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-09-30 12:03:05 -0700

    factorial using recursion

---
+++ Recursion/problems/10-factorial.js
@@ -0,0 +1,30 @@
+/***********************************************************************
+Write a recursive function called `factorial` that takes an integer, `num`,
+and returns the factorial of `num`. Assume the value of `num` is greater
+than or equal to 1.
+
+A factorial is the number you get when multiplying a number by itself minus one
+all the way down to 1 (but not 0)! We represent them with an exclamation
+point, also sometimes called a "bang" in programming.
+
+5! = 5 x 4 x 3 x 2 x 1 = 120
+***********************************************************************/
+
+function factorial(num){
+    // stop when you reach the number 1 and return 1
+    if(num < 1) return 1;
+    // multiply the number by the same number but minus 1 every time
+    return num * factorial(num - 1);
+}
+
+console.log(factorial(1)); // 1
+console.log(factorial(3)); // 6
+console.log(factorial(5)); // 120
+
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
+try {
+  module.exports = factorial;
+} catch (e) {
+  module.exports = null;
+}
