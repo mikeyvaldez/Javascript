e377dfa61a1226706231750bc4e907a64a5ddbd7
Michael Valdez <mikeyvaldez92@yahoo.com>
2025-01-31 15:26:15 -0800

    correction to count unique values

--- DSA/Patterns/MultiplePointersPattern/countUniqueValues.js
+++ DSA/Patterns/MultiplePointersPattern/countUniqueValues.js
@@ -1,9 +1,9 @@
 
 
-const countUniqueValues = (sorted_array) => {
+const countUniqueValues = (arr) => {
     if(arr.length === 0) return 0;
     let i = 0;
-    for(let j = 1; j < sorted_array.length; j++){
+    for(let j = 1; j < arr.length; j++){
         if(arr[i] !== arr[j]){
             i++;
             arr[i] = arr[j]
